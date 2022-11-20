6e2e47edb8fa4102a65563337e8251adf4cb38b0
Leobaldo Alantara Neto <leobaldoneto@gmail.com>
2022-11-20 10:54:36 -0300

    Setup static home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +25,70 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <div className={styles.content}>
+      <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.metaData}>
+          <FiCalendar />
+          <time>15 Mar 2021</time>
+          <FiUser />
+          <span>Joseph Oliveira</span>
+        </div>
+      </a>
+
+      <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.metaData}>
+          <FiCalendar />
+          <time>15 Mar 2021</time>
+          <FiUser />
+          <span>Joseph Oliveira</span>
+        </div>
+      </a>
+
+      <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.metaData}>
+          <FiCalendar />
+          <time>15 Mar 2021</time>
+          <FiUser />
+          <span>Joseph Oliveira</span>
+        </div>
+      </a>
+
+      <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.metaData}>
+          <FiCalendar />
+          <time>15 Mar 2021</time>
+          <FiUser />
+          <span>Joseph Oliveira</span>
+        </div>
+      </a>
+
+      <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.metaData}>
+          <FiCalendar />
+          <time>15 Mar 2021</time>
+          <FiUser />
+          <span>Joseph Oliveira</span>
+        </div>
+      </a>
+
+      <a onClick={console.log('ok')} className={styles.loadMore}>
+        Carregar mais posts
+      </a>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
