7de0a1c9586fc4ddabe138f161e2b922dd11d68c
Leobaldo Alantara Neto <leobaldoneto@gmail.com>
2022-11-20 21:44:43 -0300

    feat: Add post page type.

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,11 @@
+import { PrismicRichText } from '@prismicio/react';
+import { RTNode } from '@prismicio/types';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
+// import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
@@ -26,20 +29,57 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  return (
+    <>
+      {post ? (
+        <div className={styles.content}>
+          <img src={post.data.banner.url} alt={post.data.title} />
+          <div className={styles.main}>
+            <h1>{post.data.title}</h1>
+            <div className={styles.info}>
+              <FiCalendar />
+              <time>{post.first_publication_date}</time>
+              <FiUser /> <span>{post.data.author}</span>
+              <FiClock /> <span>10 min</span>
+            </div>
+            <article>
+              {post.data.content.map(content => {
+                return (
+                  <>
+                    <strong>{content.heading}</strong>
+                    <PrismicRichText
+                      field={[content.body as unknown as RTNode]}
+                    />
+                  </>
+                );
+              })}
+            </article>
+          </div>
+        </div>
+      ) : (
+        <span>Carregando...</span>
+      )}
+    </>
+  );
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient({});
+  // const posts = await prismic.getByType('posts');
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient({});
+  const post = await prismic.getByUID('posts', slug.toString());
 
-//   // TODO
-// };
+  return {
+    props: { post },
+  };
+};
