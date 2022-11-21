dd91fce6d32d7ee4a64141ef79803925f2123a2a
Leobaldo Alantara Neto <leobaldoneto@gmail.com>
2022-11-20 22:57:17 -0300

    Add return type to component function.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import styles from './header.module.scss';
 
-export default function Header() {
+export default function Header(): JSX.Element {
   return (
     <header className={styles.header}>
       <a href="/">
